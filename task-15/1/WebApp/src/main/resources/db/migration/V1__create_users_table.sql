8ab7788bda0ecac356b88043035c358e4c460052
Nikolay <blakeyh125@yandex.ru>
2026-02-15 13:40:38 +0300

    Удалил файлы

--- task-15/1/WebApp/src/main/resources/db/migration/V1__create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    email VARCHAR(100)
-);
-
-INSERT INTO users (username, email) VALUES ('Kolya', 'kolya@postgres.com');
